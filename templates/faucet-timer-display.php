a9fe6f9ea68ff20dd8d15b940b78970a97396b82
andysavage <web@directsponsor.org>
2025-06-02 16:42:22 +0100

    Make the Add New Site section collapsible and update styles

--- templates/faucet-timer-display.php
+++ templates/faucet-timer-display.php
@@ -4,9 +4,11 @@
         <p>Track your cryptocurrency faucets and PTC sites to maximize earnings!</p>
     </div>
 
-    <div class="add-site-section">
-        <h4>Add New Site</h4>
-        <form id="add-faucet-form" class="faucet-form">
+    <details class="add-site-section" open>
+        <summary class="add-site-summary">
+            Add New Site <span class="add-site-arrow" aria-hidden="true">&#x25BC;</span>
+        </summary>
+        <form id="add-faucet-form" class="faucet-form" style="margin-top:16px;">
             <div class="form-row">
                 <input type="text" id="site-name" placeholder="Site Name (e.g., FreeBitcoin)" required>
                 <input type="url" id="site-url" placeholder="Site URL" required>
@@ -14,7 +16,7 @@
                 <button type="submit">Add Site</button>
             </div>
         </form>
-    </div>
+    </details>
 
     <div class="sites-section">
         <div class="sites-header">
@@ -48,4 +50,3 @@
         </div>
     </div>
 </div>
-
